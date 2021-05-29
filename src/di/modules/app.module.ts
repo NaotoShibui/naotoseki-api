38b3fb5948be4236359cfad56596f1d7eb8968b7
naoto-seki <apron.sketch@naotoseki.com>
2021-05-29 22:13:16 +0900

    add module

--- src/di/modules/app.module.ts
+++ src/di/modules/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CityMasterModule } from './cityMster.module';
+import { DatabaseModule } from './database.module';
 import { AppController } from '../../controller/app.controller';
 import { CityMasterController } from '../../controller/cityMaster.controller';
 import { AppService } from '../../service/app.service';
 import { CityMasterService } from '../../service/cityMaster.service';
 
 @Module({
-  imports: [],
+  imports: [CityMasterModule, DatabaseModule],
   controllers: [AppController, CityMasterController],
   providers: [AppService, CityMasterService],
 })
