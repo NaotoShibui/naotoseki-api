a0d228d4861a2fdab6d73f82ac670ecb3ca52625
naoto-seki <apron.sketch@naotoseki.com>
2021-05-29 23:31:07 +0900

    add controller

--- src/di/modules/cityMst.module.ts
+++ src/di/modules/cityMst.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database.module';
 import { cityMstProviders } from '../providers/cityMst.providers';
 import { CityMstService } from '../../service/cityMst.service';
+import { CityMstController } from 'src/controller/cityMst.controller';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [CityMstController],
   providers: [
     ...cityMstProviders,
     CityMstService,
