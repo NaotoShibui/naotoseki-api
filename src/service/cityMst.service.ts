2ac811a8a31d2db79af498aff4358c34854f2709
naoto-seki <apron.sketch@naotoseki.com>
2021-05-31 15:13:20 +0900

    table修正

--- src/service/cityMst.service.ts
+++ src/service/cityMst.service.ts
@@ -54,7 +54,7 @@ export class CityMstService {
 
   async findByCity(prefecture: string, city: string): Promise<CityMstOutVo> {
     const entity: CityMstEntity = await this.cityMstRepository.createQueryBuilder(constants.CITY_MST)
-      .leftJoin('sound_archives', 'sound_archives', 'sound_archvies.prefecture = city_mst.prefecture and sound_archives.city = city_mst.city')
+      .leftJoin('sound_archives', 'sound_archives', 'sound_archives.prefecture = city_mst.prefecture and sound_archives.city = city_mst.city')
       .where(`${constants.CITY_MST}.prefecture = :prefecture`, { prefecture })
       .andWhere(`${constants.CITY_MST}.city = :city`, { city })
       .getOne();
